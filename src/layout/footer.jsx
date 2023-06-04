64dd77d28b455d74ff8bf696007d9ecf80c69f05
blueyottle <sunnydowari@gmail.com>
2023-06-04 12:58:20 +0530

    layout changes

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="container-fluid border-top mt-3 text-center bg-light text-dark py-1">
-      <p className="m-0">Copyrights reserved, developed by <b>Coden't</b>2023</p>
+      <p className="m-0">Copyrights reserved, developed by <b>Coden't </b>2023</p>
     </div>
   );
 };
